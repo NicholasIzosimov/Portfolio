a34ac4e166b5e3487b63461944d5db765759bb68
Nicholas izosimov <nicholasizosimov@Nicholass-MacBook-Pro.local>
2022-09-08 13:28:35 +0200

    Add nav bar & prepare for router

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,28 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo-sub.png'
+import { Link, NavLink } from 'react-router-dom'
+import LogoN from '../../assets/images/logo-n.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} />
-            <img src={LogoSubtitle} />
+    <div className="nav-bar">
+        <Link className="logo" to='/'>
+            <img className='logo-img' src={LogoN} alt="logo" />
+            <p className="sub-logo">Nicholas Izosimov</p>
         </Link>
+        <nav className='nav-icons'>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className="about-link" to='/about'>
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className="contact-link" to='/contact'>
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+            </NavLink>
+
+        </nav>
+        <hr className='nav-bar-divider' />
     </div>
 )
 
