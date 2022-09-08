ddf1f9237727e8d0feacea5a22edb5f8712e5605
Nicholas izosimov <nicholasizosimov@Nicholass-MacBook-Pro.local>
2022-09-08 15:36:06 +0200

    Add platform links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,19 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 import LogoN from '../../assets/images/logo-n.png'
+import linkedin from '../../assets/images/linkedin.png'
+import github from '../../assets/images/github.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
-        <Link className="logo" to='/'>
-            <img className='logo-img' src={LogoN} alt="logo" />
+        <div className="logo">
+            <Link to='/'>
+                <img className='logo-img' src={LogoN} alt="logo" />
+            </Link>
             <p className="sub-logo">Nicholas Izosimov</p>
-        </Link>
+        </div>
         <nav className='nav-icons'>
             <NavLink exact='true' activeclassname='active' to='/'>
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
@@ -20,7 +24,16 @@ const Sidebar = () => (
             <NavLink exact='true' activeclassname='active' className="contact-link" to='/contact'>
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
-
+            <ul className='platform-list'>
+                <li className='platform-box'>
+                    <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/nicholas-izosimov-43874a158/'>
+                        <img src={linkedin} color="#4d4d4e" />
+                    </a>
+                    <a target="_blank" rel='noreferrer' href='https://github.com/NicholasIzosimov'>
+                        <img src={github} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
         </nav>
         <hr className='nav-bar-divider' />
     </div>
