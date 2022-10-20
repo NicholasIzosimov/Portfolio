003312a35d9ef7ab8d7ebb395f2b248d80269e89
Nicholas izosimov <nicholasizosimov@Nicholass-MacBook-Pro.local>
2022-10-21 00:25:32 +0200

    Add Dropdown Menue for Mobile Version

--- src/components/Sidebar/Resized.js
+++ src/components/Sidebar/Resized.js
@@ -6,40 +6,51 @@ import linkedin from '../../assets/images/linkedin.png'
 import github from '../../assets/images/github.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
-const Resized = () => (
-    <>
-        <div className="resized-bar">
-            <div className="logo-resized">
-                <Link to='/'>
-                    <img className='logo-img-rezised' src={LogoN} alt="logo" />
-                </Link>
+const Resized = () => {
+
+    const [toggleState, setToggleState] = useState(false);
+
+    const handleToggle = () => {
+        setToggleState(toggleState => !toggleState);
+    }
+    let toggleClass = toggleState ? '-active' : '';
+    return (
+        <>
+            <div className="resized-bar">
+                <div className="logo-resized">
+                    <Link to='/'>
+                        <img className='logo-img-rezised' src={LogoN} alt="logo" />
+                    </Link>
+                </div>
+                <div className={`dropdown${toggleClass}`} onClick={handleToggle}>
+                        <ul className='platform-list-resized'>  
+                            <li className='platform-box-resized'>
+                                <NavLink exact='true' activeclassname='active' to='/'>
+                                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' /> Home
+                                </NavLink>
+                                <NavLink exact='true' activeclassname='active' className="about-link-rezised" to='/about'>
+                                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' /> About
+                                </NavLink>
+                                <NavLink exact='true' activeclassname='active' className="contact-link-rezised" to='/contact'>
+                                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' /> Email
+                                </NavLink>
+                                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/nicholas-izosimov-43874a158/'>
+                                    <img src={linkedin} color="#4d4d4e" alt="LinkedIn" />
+                                    <p>LinkedIn</p>
+                                </a>
+                                <a target="_blank" rel='noreferrer' href='https://github.com/NicholasIzosimov'>
+                                    <img src={github} color="#4d4d4e" alt="GitHub" />
+                                    <p>GitHub</p>
+                                </a>
+                            </li>
+                        </ul>
+                </div>
+                <button className='toggle-button' onClick={handleToggle}>_____<br />_____<br />_____</button>
             </div>
-            <nav className='nav-icons-rezised'>
-                <NavLink exact='true' activeclassname='active' to='/'>
-                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' /> Home
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="about-link-rezised" to='/about'>
-                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' /> About
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="contact-link-rezised" to='/contact'>
-                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' /> Email
-                </NavLink>
-                <ul className='platform-list-resized'>
-                    <li className='platform-box-resized'>
-                        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/nicholas-izosimov-43874a158/'>
-                            <img src={linkedin} color="#4d4d4e" alt="LinkedIn" />
-                            <p>LinkedIn</p>
-                        </a>
-                        <a target="_blank" rel='noreferrer' href='https://github.com/NicholasIzosimov'>
-                            <img src={github} color="#4d4d4e" alt="GitHub" />
-                            <p>GitHub</p>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </>
-)
+        </>
+    )
+}
 
 export default Resized
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,42 +6,47 @@ import linkedin from '../../assets/images/linkedin.png'
 import github from '../../assets/images/github.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-    <>
-    <div className='top-header'>Nicholas Izosimov</div>
-        <div className="nav-bar">
-            <div className="logo">
-                <Link to='/'>
-                    <img className='logo-img' src={LogoN} alt="logo" />
-                </Link>
-                <p className="sub-logo">Nicholas Izosimov</p>
-            </div>
-            <nav className='nav-icons'>
-                <NavLink exact='true' activeclassname='active' to='/'>
-                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' /> Home
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="about-link" to='/about'>
-                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' /> About
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="contact-link" to='/contact'>
-                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' /> Email
-                </NavLink>
-                <ul className='platform-list'>
-                    <li className='platform-box'>
-                        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/nicholas-izosimov-43874a158/'>
-                            <img src={linkedin} color="#4d4d4e" alt="LinkedIn" />
-                            <p>LinkedIn</p>
-                        </a>
-                        <a target="_blank" rel='noreferrer' href='https://github.com/NicholasIzosimov'>
-                            <img src={github} color="#4d4d4e" alt="GitHub" />
-                            <p>GitHub</p>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </>
-)
+const Sidebar = () => {
+
+
+    return (
+        <>
+            <div className='top-header'>Nicholas Izosimov</div>
+            <div className="nav-bar">
+                <div className="logo">
+                    <Link to='/'>
+                        <img className='logo-img' src={LogoN} alt="logo" />
+                    </Link>
+                    <p className="sub-logo">Nicholas Izosimov</p>
+                </div>
+                    <nav className='nav-icons'>
+                        <NavLink exact='true' activeclassname='active' to='/'>
+                            <FontAwesomeIcon icon={faHome} color='#4d4d4e' /> Home
+                        </NavLink>
+                        <NavLink exact='true' activeclassname='active' className="about-link" to='/about'>
+                            <FontAwesomeIcon icon={faUser} color='#4d4d4e' /> About
+                        </NavLink>
+                        <NavLink exact='true' activeclassname='active' className="contact-link" to='/contact'>
+                            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' /> Email
+                        </NavLink>
+                        <ul className='platform-list'>
+                            <li className='platform-box'>
+                                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/nicholas-izosimov-43874a158/'>
+                                    <img src={linkedin} color="#4d4d4e" alt="LinkedIn" />
+                                    <p>LinkedIn</p>
+                                </a>
+                                <a target="_blank" rel='noreferrer' href='https://github.com/NicholasIzosimov'>
+                                    <img src={github} color="#4d4d4e" alt="GitHub" />
+                                    <p>GitHub</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+        </>
+    )
+}
 
 export default Sidebar
